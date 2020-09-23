5fc0e41143e8f720b587e5906a213cb984117178
Eugenia <eugeniasol13@gmail.com>
2020-09-23 15:22:58 -0400

    fist push database

---
+++ scheema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  dep_name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  dep_id INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (dep_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES manager(id)
+);
